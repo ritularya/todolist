e4bf444ccf6fa62e5f48947d84e1cf5ac141eff7
Ritul Arya <ritul.arya128@gmail.com>
2023-09-15 16:05:54 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'To-Do List';
   activity = ["Hunting", "Homework"];
-  fish = ["Maths", "OS", "Computer Networks"];
+  sub = ["Maths", "OS", "Computer Networks"];
   animals = ["Bear", "Boar", "Tiger"];
   message: string = "Hello!!";
   inputValue : string = "Initial String";
